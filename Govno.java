1ec573fa1c007c639193382e85a396802b530e47
Nyan <akinaro666@gmail.com>
2025-05-16 23:54:57 +0700

    Первое изменение

--- Govno.java
+++ Govno.java
@@ -2,7 +2,7 @@ public class Govno {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println("Яна Солях");
-            //Привеgity т
+            //первое измененеи 
         }
     }
 }
