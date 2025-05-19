9ca53012225c3aeb597930f5e4d7e86a9ce7af07
Nyan <akinaro666@gmail.com>
2025-05-19 20:53:04 +0700

    проверка ребейс с конфликтами

--- Example.java
+++ Example.java
@@ -1,7 +1,7 @@
 public class Example {
     public static void main(String[] args) {
         System.out.println("Привет это новая ветка ехехехе");
-        //change11111
+        //change11111123123крутяк
         //крутяк
     }
 }
