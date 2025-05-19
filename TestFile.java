007be4741dde9420dae05b52b7df77b1ed2e9bca
Nyan <akinaro666@gmail.com>
2025-05-19 17:26:41 +0700

    user 2 chanched Testfile

--- TestFile.java
+++ TestFile.java
@@ -2,6 +2,7 @@ public class TestFile {
     public static void main(String[] args) {
         for (int i = 0; i < 199; i++) {
             System.out.println("Привет 198 раз");
+            //12313
         }
     }
 }
